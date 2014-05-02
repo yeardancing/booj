47b2aa0a33c15f43cdb8addbf0c6af25002c4b3f
yeardancing <derek.spadaro@gmail.com>
2014-05-02 02:45:00 +0000

    need to add some more unit tests for the player

--- test/player/testplayer.sh
+++ test/player/testplayer.sh
@@ -1,3 +1,2 @@
 cd ../../..
-#PYTHONPATH=. python booj/test/player/playerHarness.py '/media/sf_Music/New Order/Senses Working Overtime/01 - Age of Consent.ogg'
-PYTHONPATH=. python booj/test/player/playerHarness.py /home/pi/src/python/19_Hand_In_Glove.mp3
+PYTHONPATH=. python booj/test/player/playerHarness.py booj/19_Hand_In_Glove.mp3
