61c846b8910d1dd919a2b0c560c281f1e63fe113
Derek Spadaro <derek.spadaro@gmail.com>
2016-12-24 12:19:01 -0600

    Use pagebeforeshow for passing data inter-page.

--- static/controls.js
+++ static/controls.js
@@ -1,10 +1,10 @@
-
 function boojPageInit() {
 	if (typeof songInfo === "undefined") {
-		window.songInfo = {
+		songInfo = {
 			title			: "none",
             isplaying 		: "false",
-			duration 		: null
+			duration 		: null,
+            sliderValue     : 1
 		};
 	}
 
@@ -14,10 +14,6 @@ function boojPageInit() {
 
 function incrementSlider() {
     var currSliderVal = $('input#slider-0').slider("option", "value");
-    if (currSliderVal == null)
-    {
-        currSliderVal = 1;
-    }
     currSliderVal += 1;
     $('input#slider-0').slider("option", "value", currSliderVal);
     $('input[type="number"]#slider-0').val(currSliderVal).slider("refresh");
@@ -27,24 +23,25 @@ function updatePlayButton(isplaying) {
     playbutton = $('button#playbutton.ctrls');
     if (isplaying === "true")
     {
-        if (window.sliderTimer)
-        {
-            clearInterval(window.sliderTimer);
-        }
-        window.sliderTimer = setInterval(incrementSlider, 1000);
+        //if (window.sliderTimer)
+        //{
+        //    clearInterval(window.sliderTimer);
+        //}
+        //window.sliderTimer = setInterval(incrementSlider, 1000);
         playbutton.text("Stop");
         playbutton.removeClass('ui-icon-arrow-r').addClass('ui-icon-delete');
         nowplaying = true
     }
     else
     {
-        clearInterval(window.sliderTimer);
+        //clearInterval(window.sliderTimer);
         playbutton.text("Play");
         playbutton.removeClass('ui-icon-delete').addClass('ui-icon-arrow-r');
         nowplaying = false
     }
 }
 
+// play button logic (song page only)
 $('.play').bind('click', function(event, ui) {
     console.log('was clicked for song ' + this.id.substring(5) );
     console.log(document.location.toString().toLowerCase());
@@ -55,18 +52,18 @@ $('.play').bind('click', function(event, ui) {
         if (data.playing)
         {
             updatePlayButton(data.playing);
-            songInfo.isplaying = data.playing;
         }
         if (data.artist && data.song)
         {
-            songInfo.title = data.artist + ' - ' + data.song;
-            $('h1#currentsong').text(songInfo.title);
+            var title = data.artist + ' - ' + data.song;
+            $('h1#currentsong').text(title);
 
         }
         if (data.duration)
         {
             // TODO
-            songInfo.duration = data.duration;
+            var duration = data.duration;
+            var sliderValue = 1;
             console.log('setting max val to ' + data.duration);
             //$('input#slider-0').slider('option', 'max', data.duration);
             $('input#slider-0').attr('max', data.duration);
@@ -78,13 +75,14 @@ $('.play').bind('click', function(event, ui) {
     return true;
 });
 
+// bottom panel controls logic 
 $('button#playbutton.ctrls').bind('click', function(event, ui) {
-    if (songInfo.isplaying)
-    {
+    var button = $('button#playbutton.ctrls').text();
+    console.log('button=' + button);
+    if (button === "Stop" || button === "StopStop") {
         var postdata = 'playnow=false';  // post the form values via ajax
     }
-    else
-    {
+    else {
         var postdata = 'playnow=true';  // post the form values via ajax
     }
     currentPageUrl = document.location.toString().toLowerCase();
@@ -92,10 +90,36 @@ $('button#playbutton.ctrls').bind('click', function(event, ui) {
         if (data.playing && data.position)
         {
             console.log('data.position: ' + data.position);
+            console.log('data.playing: ' + data.playing);
             updatePlayButton(data.playing);
-            songInfo.isplaying = data.playing;
         }
     });
     return true;
+});
+
+
+// get slider position from previous page
+$(document).on('pagebeforeshow', function(e, data){     
+    var button = data.prevPage.find('button#playbutton.ctrls').text();
+    var isplaying = "false";
+    if (button === "Stop") {
+        isplaying = "true";
+    }
+    updatePlayButton(isplaying);
+
+    var title = data.prevPage.find('h1#currentsong').text();
+    $('h1#currentsong').text(title);
+    /*
+    if (sliderValue === undefined) {
+        console.log('previous value was undefined')
+    }
+    else {
+        console.log('prev page val is ' + sliderValue);
+    }
+    sliderValue = $('input#slider-0').slider("option", "value");
+    console.log('this page val is ' + sliderValue);
+    $('input#slider-0').slider("option", "value", sliderValue);
+    $('input[type="number"]#slider-0').val(sliderValue).slider("refresh");
+    */
 });
 
